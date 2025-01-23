fbc132501a329e357c2f35eac2ac61b430064109
Christian Contreras Campana <chrisjcc.physics@gmail.com>
2025-01-24 00:47:28 +0100

    Preparation for inference with pre-trained agents

--- examples/runner.py
+++ examples/runner.py
@@ -2,11 +2,41 @@ import os
 import yaml
 import ray
 import argparse
-import replicantdrivesim
+
+from collections import OrderedDict
+
+import torch
+
+from ray.rllib.policy.policy import Policy
+from ray.rllib.policy.sample_batch import SampleBatch
+
 from mlagents_envs.exception import UnityCommunicatorStoppedException
+
+import replicantdrivesim
+
 
 def run_episodes(env, num_episodes):
     """Run a defined number of episodes with the environment."""
+    # Print available behavior names
+    behavior_names = list(env.behavior_specs.keys())
+    print(f"Available behavior names: {behavior_names}")
+
+    # Load the trained DQN policy
+    base_directory = "/Users/christiancontrerascampana/ray_results"
+    #run = "PPO_2024-06-06_12-52-47/PPO_highway_traffic_e9492_00000_0_2024-06-06_12-52-47"
+    #checkpoint = "checkpoint_000000"
+    #checkpoint_path = os.path.join(base_directory, run, checkpoint)
+    #print(f"CHECKPOINT PATH: {checkpoint_path}")
+
+    # Get the policy object, use the `from_checkpoint` utility of the Policy class:
+    #print(Policy.from_checkpoint(checkpoint_path))
+    #policy = Policy.from_checkpoint(checkpoint_path)["policy_1"]
+    #policy.export_model(f"{checkpoint}_saved_model")
+
+    # Get the TensorFlow model
+    #model =  policy.model
+    #print(f"{model.base_model.summary()}")
+
     try:
         for episode in range(num_episodes):
             print(f"Starting episode {episode + 1}")
@@ -15,6 +45,28 @@ def run_episodes(env, num_episodes):
             done = False
 
             while not done:
+
+                # Preprocess observations
+                #preprocessed_observations = {}
+                #for agent, observation in observations.items():
+                #    preprocessed_observations[agent] = torch.tensor(observation, dtype=torch.float32)
+
+                # Flatten the observations dictionary into a single tensor
+                #flattened_observations = torch.cat(list(preprocessed_observations.values()), dim=0)
+
+                # Reshape the flattened observations to match the expected input shape
+                #expected_input_shape = (2, 8)  # Replace with the expected input shape
+                #reshaped_observations = flattened_observations.view(expected_input_shape)
+
+                # Create a SampleBatch from the preprocessed observations
+                #input_dict = SampleBatch({
+                #    "obs": reshaped_observations,
+                #    # Add any other necessary keys here
+                #})
+
+                # Compute actions for all agents using compute_actions_from_input_dict
+                #action_dict, _, _ = policy.compute_actions_from_input_dict(input_dict)
+
                 actions = env.action_space_sample()
 
                 # Modify the actions for all agents
@@ -31,7 +83,11 @@ def run_episodes(env, num_episodes):
 
                 # Step the environment
                 observations, rewards, terminateds, truncateds, infos = env.step(actions)
-                print("rewards: ", rewards)
+
+                for agent in rewards:
+                    total_reward = rewards[agent]
+                    print(f"Rewards for {agent}: {total_reward}")
+                    print(f"Reward components for {agent}: {infos[agent]}")
 
                 # Check if the episode is done
                 done = terminateds.get("__all__", False) or truncateds.get("__all__", False)
