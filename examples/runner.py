7c5db3617deef519772968dbedfb0f3a104e89d2
Christian Contreras Campana <chrisjcc.physics@gmail.com>
2025-01-26 22:39:51 +0100

    Fix code

--- examples/runner.py
+++ examples/runner.py
@@ -1,22 +1,12 @@
-import os
-import yaml
 import argparse
-
-from collections import OrderedDict
-
-import torch
+import os
 
 import ray
-from ray.rllib.policy.policy import Policy
-from ray.rllib.policy.sample_batch import SampleBatch
-
-<<<<<<< Updated upstream
-=======
-from ray.rllib.policy.policy import Policy
-from ray.rllib.policy.sample_batch import SampleBatch
-
->>>>>>> Stashed changes
+import torch
+import yaml
 from mlagents_envs.exception import UnityCommunicatorStoppedException
+from ray.rllib.policy.policy import Policy
+from ray.rllib.policy.sample_batch import SampleBatch
 
 import replicantdrivesim
 
@@ -29,19 +19,19 @@ def run_episodes(env, num_episodes):
 
     # Load the trained DQN policy
     base_directory = "/Users/christiancontrerascampana/ray_results"
-    #run = "PPO_2024-06-06_12-52-47/PPO_highway_traffic_e9492_00000_0_2024-06-06_12-52-47"
-    #checkpoint = "checkpoint_000000"
-    #checkpoint_path = os.path.join(base_directory, run, checkpoint)
-    #print(f"CHECKPOINT PATH: {checkpoint_path}")
+    # run = "PPO_2024-06-06_12-52-47/PPO_highway_traffic_e9492_00000_0_2024-06-06_12-52-47"
+    # checkpoint = "checkpoint_000000"
+    # checkpoint_path = os.path.join(base_directory, run, checkpoint)
+    # print(f"CHECKPOINT PATH: {checkpoint_path}")
 
     # Get the policy object, use the `from_checkpoint` utility of the Policy class:
-    #print(Policy.from_checkpoint(checkpoint_path))
-    #policy = Policy.from_checkpoint(checkpoint_path)["policy_1"]
-    #policy.export_model(f"{checkpoint}_saved_model")
+    # print(Policy.from_checkpoint(checkpoint_path))
+    # policy = Policy.from_checkpoint(checkpoint_path)["policy_1"]
+    # policy.export_model(f"{checkpoint}_saved_model")
 
     # Get the TensorFlow model
-    #model =  policy.model
-    #print(f"{model.base_model.summary()}")
+    # model =  policy.model
+    # print(f"{model.base_model.summary()}")
 
     try:
         for episode in range(num_episodes):
@@ -53,25 +43,25 @@ def run_episodes(env, num_episodes):
             while not done:
 
                 # Preprocess observations
-                #preprocessed_observations = {}
-                #for agent, observation in observations.items():
+                # preprocessed_observations = {}
+                # for agent, observation in observations.items():
                 #    preprocessed_observations[agent] = torch.tensor(observation, dtype=torch.float32)
 
                 # Flatten the observations dictionary into a single tensor
-                #flattened_observations = torch.cat(list(preprocessed_observations.values()), dim=0)
+                # flattened_observations = torch.cat(list(preprocessed_observations.values()), dim=0)
 
                 # Reshape the flattened observations to match the expected input shape
-                #expected_input_shape = (2, 8)  # Replace with the expected input shape
-                #reshaped_observations = flattened_observations.view(expected_input_shape)
+                # expected_input_shape = (2, 8)  # Replace with the expected input shape
+                # reshaped_observations = flattened_observations.view(expected_input_shape)
 
                 # Create a SampleBatch from the preprocessed observations
-                #input_dict = SampleBatch({
+                # input_dict = SampleBatch({
                 #    "obs": reshaped_observations,
                 #    # Add any other necessary keys here
-                #})
+                # })
 
                 # Compute actions for all agents using compute_actions_from_input_dict
-                #action_dict, _, _ = policy.compute_actions_from_input_dict(input_dict)
+                # action_dict, _, _ = policy.compute_actions_from_input_dict(input_dict)
 
                 actions = env.action_space_sample()
 
@@ -88,7 +78,9 @@ def run_episodes(env, num_episodes):
                 print(f"actions: {actions}")
 
                 # Step the environment
-                observations, rewards, terminateds, truncateds, infos = env.step(actions)
+                observations, rewards, terminateds, truncateds, infos = env.step(
+                    actions
+                )
 
                 for agent in rewards:
                     total_reward = rewards[agent]
@@ -96,7 +88,9 @@ def run_episodes(env, num_episodes):
                     print(f"Reward components for {agent}: {infos[agent]}")
 
                 # Check if the episode is done
-                done = terminateds.get("__all__", False) or truncateds.get("__all__", False)
+                done = terminateds.get("__all__", False) or truncateds.get(
+                    "__all__", False
+                )
 
             print(f"Episode {episode + 1} finished")
 
@@ -108,20 +102,21 @@ def run_episodes(env, num_episodes):
         print("Closing the environment.")
         env.close()
 
+
 def main():
     # Set up argument parser
     parser = argparse.ArgumentParser(description="Run a Unity environment simulation.")
     parser.add_argument(
         "--num-episodes",
-        type=int, 
+        type=int,
         default=5,
-        help="Number of episodes to run (default: 10)"
+        help="Number of episodes to run (default: 10)",
     )
     parser.add_argument(
-       "--config-path",
-       type=str,
-       default=os.path.join("examples", "configs", "config.yaml"),
-       help="Environment configuration."
+        "--config-path",
+        type=str,
+        default=os.path.join("examples", "configs", "config.yaml"),
+        help="Environment configuration.",
     )
     # Parse command-line arguments
     args = parser.parse_args()
@@ -138,7 +133,9 @@ def main():
 
     try:
         # Create the Unity environment
-        env = replicantdrivesim.make(env_name="replicantdrivesim-v0", config=config_data)
+        env = replicantdrivesim.make(
+            env_name="replicantdrivesim-v0", config=config_data
+        )
 
         # Run the episodes
         run_episodes(env, num_episodes)
@@ -147,6 +144,7 @@ def main():
     finally:
         # Shutdown Ray
         ray.shutdown()
+
 
 if __name__ == "__main__":
     main()
